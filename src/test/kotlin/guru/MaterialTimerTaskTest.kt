b57209abf8b404558bba276e6de51ca44e9649bb
Andriy Kryvtsun <kontiky@gmail.com>
2024-11-02 21:32:07 +0200

    #14 add coverage

--- src/test/kotlin/guru/MaterialTimerTaskTest.kt
+++ src/test/kotlin/guru/MaterialTimerTaskTest.kt
@@ -4,6 +4,8 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo
 import org.telegram.telegrambots.meta.generics.TelegramClient
 import kotlin.test.Test
 
@@ -15,16 +17,24 @@ class MaterialTimerTaskTest {
 
         val t = MaterialTimerTask(
             user = 1234L,
-            items = listOf(TextItem("hello")),
+            items = listOf(
+                TextItem("hello text"),
+                ImageItem("course/image.png"),
+                VideoItem("course/video.mp4"),
+            ),
             client = tgClient,
             blockAction = { user -> println(user) }
         )
 
         every { tgClient.execute(any<SendMessage>()) } returns null
+        every { tgClient.execute(any<SendPhoto>()) } returns null
+        every { tgClient.execute(any<SendVideo>()) } returns null
 
         t.run()
 
         verify { tgClient.execute(any<SendMessage>()) }
+        verify { tgClient.execute(any<SendPhoto>()) }
+        verify { tgClient.execute(any<SendVideo>()) }
     }
 
 }
