cd866daa949790db42bf8280857287021247e4db
Andriy Kryvtsun <kontiky@gmail.com>
2024-11-05 11:18:06 +0200

    #14 improve StateStorageTest

--- src/test/kotlin/guru/state/StateStorageTest.kt
+++ src/test/kotlin/guru/state/StateStorageTest.kt
@@ -1,8 +1,11 @@
 package guru.state
 
+import guru.MaterialTimerTask
+import guru.state.BotState.CourseState
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.time.LocalDateTime
 
 class StateStorageTest {
 
@@ -19,10 +22,13 @@ class StateStorageTest {
 
     @Test
     fun `save empty state`() {
+        var userId = 11111L
+        val users = mapOf(userId to CourseState(LocalDateTime.now(), emptyList<MaterialTimerTask>()))
+
         val storageName = "test"
         val storage = StateStorage(storageName)
 
-        storage.save(emptyMap())
+        storage.save(users)
 
         val state = File(storageName)
         assertThat(state).exists()
